5d0c5c6379c320b91f8a2dae751e71b0a4501f57
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-09-18 17:42:08 -0600

    Create blended_performance.sql

---
+++ models/reporting/blended_performance.sql
@@ -0,0 +1,30 @@
+{{ config (
+    alias = target.database + '_blended_performance'
+)}}
+
+WITH paid_data as
+    (SELECT channel, date::date, date_granularity, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
+        COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue
+    FROM
+        (SELECT 'Meta' as channel, date, date_granularity, 
+            spend, link_clicks as clicks, impressions, purchases, revenue
+        FROM {{ source('reporting','facebook_ad_performance') }}
+        UNION ALL
+        SELECT 'Google Ads' as channel, date, date_granularity,
+            spend, clicks, impressions, purchases, revenue
+        FROM {{ source('reporting','googleads_campaign_performance') }}
+        UNION ALL
+        SELECT 'Bing' as channel, date, date_granularity,
+            spend, clicks, impressions, purchases, revenue
+        FROM {{ source('reporting','bingads_campaign_performance') }})
+    GROUP BY channel, date, date_granularity)
+    
+SELECT channel,
+    date,
+    date_granularity,
+    spend,
+    clicks,
+    impressions,
+    purchases,
+    revenue
+FROM paid_data
